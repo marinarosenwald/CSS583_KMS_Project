fd64c961287854af9888eae1ca799c75e5ced60c
njewel <nvjewel@gmail.com>
2024-01-21 23:00:00 -0800

    Starter code to run local MML

---
+++ LLM-API/orca-3b-main.py
@@ -0,0 +1,9 @@
+from llama_cpp import Llama
+llm = Llama(model_path="./models/orca-mini-3b.ggmlv3.q4_1.bin")
+instruction = input("User: ")
+
+system = 'you are ai system'
+prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response: \n"
+output = llm(prompt,temperature = 0.7, max_tokens=512,top_k=20, top_p=0.9, repeat_penalty=1.15)
+res = output['choices'][0]['text'].strip()
+print('Orca3b: ' + res)
