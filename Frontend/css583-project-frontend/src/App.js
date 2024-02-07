3025cbf85464819e0f4ba5271a39db87b07fd7d1
marinarosenwald <marinarosenwald@gmail.com>
2024-02-06 19:00:28 -0800

    initial website upload, bugs I need to fix

---
+++ Frontend/css583-project-frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from "react"; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import "./styles.css"
+
+import Navbar from "./components/Navbar";
+import CSS583Project from "./pages/CSS583Project";
+
+
+
+  function App() {
+    return (
+      <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+
+          <Route path='/' element={<CSS583Project/>} />
+        </Routes>
+      </Router>
+      </div>
+    );
+  }
+    
+  export default App;
+
+    
+
+
---
+++ Frontend/css583-project-frontend/src/pages/CSS583Project.js
@@ -0,0 +1,21 @@
+import React from "react"; 
+
+import "./CSS583ProjectStyle.css";
+import "../components/displayBox.css"; 
+
+// function Box({ children, ...props }) {
+//   return <div {...props}>{children}</div>
+// }
+
+
+function CSS583Project() {
+  return (
+    <div id="CSS583Project" >
+    
+
+  </div>
+    
+  )
+}
+
+export default CSS583Project;
