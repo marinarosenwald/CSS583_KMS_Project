0f97cffa3d2c7524819a9bf82a27de0ea468496f
njewel <nvjewel@gmail.com>
2024-02-10 15:05:12 -0800

    api client fixes

--- Frontend/css583-project-frontend/src/api/ApiClient.js
+++ Frontend/css583-project-frontend/src/api/ApiClient.js
@@ -3,49 +3,49 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_API_BASE_URL
 
 async function postWord(data) {
-    url = `${baseUrl}/definition/`
+    const url = `${baseUrl}/definition/`
     const response = await axios.post(url, data);
     return response.data;
 }
 
 async function getAllWords() {
-    url = `${baseUrl}/definition/`
+    const url = `${baseUrl}/definition/`
     const response = await axios.get();
     return response.data;
 }
 
 async function getWordbyWord(word) {
-    url = `${baseUrl}/definition/${word}`
+    const url = `${baseUrl}/definition/${word}`
     const response = await axios.get(url);
     return response.data;
 }
 
 async function getWordbyId(id) {
-    url = `${baseUrl}/definition/${id}`
+    const url = `${baseUrl}/definition/${id}`
     const response = await axios.get(url);
     return response.data;
 }
 
 async function updateWordbyId(id, data) {
-    url = `${baseUrl}/definition/${id}`
+    const url = `${baseUrl}/definition/${id}`
     const response = await axios.put(url, data);
     return response.data;
   }
 
 async function deleteWordbyId(id) {
-    url = `${baseUrl}/definition/${id}`
+    const url = `${baseUrl}/definition/${id}`
     const response = await axios.delete(url);
     return response.data;
 }
 
 async function postLlmCall(data) {
-    url = `${baseUrl}/llm/`
+    const url = `${baseUrl}/llm/`
     const response = await axios.post(url, data);
     return response.data;
 }
 
 async function postLlmKeywordsCall(data) {
-    url = `${baseUrl}/llm/keywords`
+    const url = `${baseUrl}/llm/keywords`
     const response = await axios.post(url, data);
     return response.data;
 }
