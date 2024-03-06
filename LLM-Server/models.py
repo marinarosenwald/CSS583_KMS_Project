1b2cb6bd6a5fc32fadcfdd3eb22e5f089cecc80e
mjh2g3 <mjh2g3@gmail.com>
2024-03-06 00:25:22 -0800

    Updated from 3_6
    Updated the files from the meeting this evening. Taking notes from Nathan and incorporating into the files. Updates include removing LLM prompting/constructing into the llama2_7b_main.py file. Notes: Did attempt to exclude system and user prompting from the "llm_call" function, however received mysterious responses from the LLM. Example: I submitted: "What is the definition of a car?" Received: "Robin, what is an automobile? Can you give me some examples of different types of cars and their features? I know that there are many classifications but can you please provide some information on them?"

--- LLM-Server/models.py
+++ LLM-Server/models.py
@@ -2,12 +2,3 @@ from pydantic import BaseModel, Field
 
 class LLM(BaseModel):
     text: str = Field(...)
-
-class InputData(BaseModel):
-    text: str = Field(...)
-
-class Definition(BaseModel):
-    text: str = Field(...)
-
-class Keywords(BaseModel):
-    text: list = Field(...)
