bf06f619f088d12cdb383caacdf35629b3053896
marinarosenwald <marinarosenwald@gmail.com>
2024-03-05 18:49:48 -0800

    frontend

--- Frontend/css583-project-frontend/src/pages/CSS583Project.js
+++ Frontend/css583-project-frontend/src/pages/CSS583Project.js
@@ -1,13 +1,55 @@
-import React from "react"; 
-
-import "./CSS583ProjectStyle.css";
-import "../components/displayBox.css"; 
+import React, { useState } from 'react';
+import './CSS583ProjectStyle.css';
 
 function CSS583Project() {
+  const [options, setOptions] = useState([
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+    // Add more options as needed
+  ]);
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClick = (index) => {
+    setSelectedOption(index === selectedOption ? null : index);
+  };
+
+  const [userInput, setUserInput] = useState('');
+
+  const handleUserInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
-    <div id="CSS583Project" >
-  </div>
-  )
+    <div id="CSS583Project">
+      <div className="search-container">
+        <input type="text" className="search-bar" placeholder="Search..." />
+      </div>
+      <div className="side-panel-left">
+        <ul>
+          {options.map((option, index) => (
+            <li
+              key={index}
+              className={index === selectedOption ? 'selected' : ''}
+              onClick={() => handleOptionClick(index)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="side-panel-right">
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleUserInputChange}
+          placeholder="Enter your text here..."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CSS583Project;
